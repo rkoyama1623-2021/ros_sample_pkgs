cd662b242e5480f55a1cce38a0e3644349e3bc35
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2016-07-15 21:00:17 +0900

    [beginner_tutorials/package.xml,CMakeList.txt,cfg,scripts/rqt_reconfigyre_sample.py] add rqt_reconfigure sample

---
+++ beginner_tutorials/scripts/rqt_reconfigyre_sample.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rospy
+from dynamic_reconfigure.server import Server
+from beginner_tutorials.cfg import TutorialsConfig
+
+def callback(config, level):
+    rospy.loginfo("""Reconfigure Request: {int_param}, {double_param},{str_param}, {bool_param}, {size}""".format(**config))
+    return config
+
+if __name__ == "__main__":
+    rospy.init_node("dynamic_tutorials", anonymous = True)
+    srv = Server(TutorialsConfig, callback)
+    rospy.spin()
